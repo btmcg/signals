f99732dd832dd85b99c9fbb087c3d7b3fdec4783
brian <brian@eolas>
2020-06-06 19:55:18 -0400

    [mk] delete makefiles

--- mk/version.mk
+++
@@ -1,9 +0,0 @@
-GIT := /usr/bin/git
-VERSION := $(shell $(GIT) describe --abbrev=4 --always --dirty --tags)
-VERSION_FILE := src/version.hpp
-
-all: $(VERSION_FILE)
-
-$(VERSION_FILE): .git/HEAD .git/index
-	@echo "#pragma once" > $@
-	@echo "constexpr char const* VERSION = \"$(VERSION)\";" >> $@
